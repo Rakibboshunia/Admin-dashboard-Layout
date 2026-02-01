dfcbe25cc4e98e0c84670d4693f8da7373aa377d
Rakib Boshunia <fireaiboshunia.info@gmail.com>
2026-02-01 12:20:32 +0600

    Dashboard Layout done

---
+++ src/router/router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import DashboardLayout from "../layout/DashboardLayout";
+import Home from "../pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      }
+    ],
+  },
+]);
+
+export default router;
